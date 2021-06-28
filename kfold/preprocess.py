28c63bbd4b7cf614e763d2ed5d893df3092ecfdb
mz256 <82032354+mz256@users.noreply.github.com>
2021-06-28 21:21:06 +0200

    Added files

---
+++ kfold/preprocess.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import os
+import gc
+import numpy as np
+import pandas as pd
+import datetime
+from utils import *
+#from utils import delete_bad_sitezero, extract_temporal
+
+
+def load_data(source='train'):
+    assert source in ['train', 'test']
+    df = pd.read_csv(f'{DATA_PATH}/{source}.csv', parse_dates=['timestamp'])
+    return reduce_mem(df)
+
+
+def load_building():
+    df = pd.read_csv(f'{DATA_PATH}/building_metadata.csv').fillna(-1)
+    return reduce_mem(df)
+
+
+def load_weather(source='train', fix_timezone=True, impute=True, add_lag=True):
+    assert source in ['train', 'test']
+    df = pd.read_csv(f'{DATA_PATH}/weather_{source}.csv', parse_dates=['timestamp'])
+    if fix_timezone:
+        offsets = [5, 0, 9, 6, 8, 0, 6, 6, 5, 7, 8, 6, 0, 7, 6, 6]
+        offset_map = {site: offset for site, offset in enumerate(offsets)}
+        df.timestamp = df.timestamp - pd.to_timedelta(df.site_id.map(offset_map), unit='h')
+    if impute:
+        site_dfs = []
+        for site in df.site_id.unique():
+            if source == 'train':
+                new_idx = pd.date_range(start='2016-1-1', end='2016-12-31-23', freq='H')
+            else:
+                new_idx = pd.date_range(start='2017-1-1', end='2018-12-31-23', freq='H')
+            site_df = df[df.site_id == site].set_index('timestamp').reindex(new_idx)
+            site_df.site_id = site
+            for col in [c for c in site_df.columns if c != 'site_id']:
+                site_df[col] = site_df[col].interpolate(limit_direction='both', method='linear')
+                site_df[col] = site_df[col].fillna(df[col].median())
+            site_dfs.append(site_df)
+        df = pd.concat(site_dfs)
+        df['timestamp'] = df.index
+        df = df.reset_index(drop=True)
+
+    if add_lag:
+        df = add_lag_features(df, window=3)
+
+    return reduce_mem(df)
+
+
+def merged_dfs(source='train', fix_timezone=True, impute=True, add_lag=False):
+    df = load_data(source=source).merge(load_building(), on='building_id', how='left')
+    df = df.merge(load_weather(source=source, fix_timezone=fix_timezone, impute=impute, add_lag=add_lag),
+                  on=['site_id', 'timestamp'], how='left')
+    if source == 'train':
+        X = df.drop('meter_reading', axis=1)
+        y = np.log1p(df.meter_reading)  # log-transform of target
+        return X, y
+    elif source == 'test':
+        return df
+
+
+if __name__ == '__main__':
+    # TO DO: can add here arg with argparser, for lag vs no lag
+    """
+    python preprocess.py --nolag
+    python preprocess.py
+    """
+
+    # # TRAINING DATASET
+    X_train, y_train = merged_dfs(add_lag=False)
+
+    # preprocessing
+    X_train, y_train = delete_bad_sitezero(X_train, y_train)
+    X_train = extract_temporal(X_train)
+
+    # remove timestamp and other unimportant features
+    to_drop = ['timestamp', 'sea_level_pressure', 'wind_direction', 'wind_speed']
+    X_train.drop(to_drop, axis=1, inplace=True)
+    gc.collect()
+
+    df_train = pd.concat([X_train, y_train], axis=1)
+    del X_train, y_train
+    gc.collect()
+
+    df_train.info()
+
+    # save in HDF5
+    df_train.to_hdf(f'{DATA_PATH}/preprocessed/lgb_no_lag.h5', index=False, key='train', mode='w')
+    del df_train
+
+    # # TEST DATASET
+
+    X_test = merged_dfs(source='test', add_lag=False)
+    X_test = extract_temporal(X_test, train=False)
+    X_test.drop(columns=['timestamp'] + to_drop, inplace=True)
+    gc.collect()
+
+    X_test.info()
+
+    X_test.to_hdf(f'{DATA_PATH}/preprocessed/lgb_no_lag.h5', index=False, key='test', mode='w')
--- kfold/utils.py
+++ kfold/utils.py
@@ -1,10 +1,15 @@
-#!/usr/bin/env python
-
 import os
 import gc
 import numpy as np
 import pandas as pd
 import datetime
+import json
+
+# load file paths
+settings = json.load(open("/Users/michele/github/ashrae/settings.json"))
+OUTPUT_PATH = settings["OUTPUT_PATH"]
+MODEL_PATH = settings["MODEL_PATH"]
+DATA_PATH = settings["DATA_PATH"]
 
 def reduce_mem(df):
     result = df.copy()
@@ -23,6 +28,7 @@ def reduce_mem(df):
                 result[col] = pd.to_numeric(col_data, downcast='float')
     return result
 
+
 def add_lag_features(weather_df, window=3):
     group_df = weather_df.groupby('site_id')
     cols = ['air_temperature', 'cloud_coverage', 'dew_temperature', 'precip_depth_1_hr']
@@ -36,13 +42,15 @@ def add_lag_features(weather_df, window=3):
         weather_df[f'{col}_max_lag{window}'] = lag_max[col]
     return weather_df
 
-def _delete_bad_sitezero(X, y):
+
+def delete_bad_sitezero(X, y):
     bad_rows = X[(X.timestamp <= '2016-05-20') & (X.site_id == 0) & (X.meter == 0)].index
     X = X.drop(index=bad_rows)
     y = y.reindex_like(X)
     return X.reset_index(drop=True), y.reset_index(drop=True)
 
-def _extract_temporal(X, train=True):
+
+def extract_temporal(X, train=True):
     X['hour'] = X.timestamp.dt.hour
     X['weekday'] = X.timestamp.dt.weekday
     if train:
@@ -51,54 +59,4 @@ def _extract_temporal(X, train=True):
     # month and year cause overfit, could try other (holiday, business, etc.)
     return reduce_mem(X)
 
-def load_data(source='train'):
-    assert source in ['train', 'test']
-    df = pd.read_csv(f'{path}/{source}.csv', parse_dates=['timestamp'])
-    return reduce_mem(df)
 
-
-def load_building():
-    df = pd.read_csv(f'{path}/building_metadata.csv').fillna(-1)
-    return reduce_mem(df)
-
-
-def load_weather(source='train', fix_timezone=True, impute=True, add_lag=True):
-    assert source in ['train', 'test']
-    df = pd.read_csv(f'{path}/weather_{source}.csv', parse_dates=['timestamp'])
-    if fix_timezone:
-        offsets = [5, 0, 9, 6, 8, 0, 6, 6, 5, 7, 8, 6, 0, 7, 6, 6]
-        offset_map = {site: offset for site, offset in enumerate(offsets)}
-        df.timestamp = df.timestamp - pd.to_timedelta(df.site_id.map(offset_map), unit='h')
-    if impute:
-        site_dfs = []
-        for site in df.site_id.unique():
-            if source == 'train':
-                new_idx = pd.date_range(start='2016-1-1', end='2016-12-31-23', freq='H')
-            else:
-                new_idx = pd.date_range(start='2017-1-1', end='2018-12-31-23', freq='H')
-            site_df = df[df.site_id == site].set_index('timestamp').reindex(new_idx)
-            site_df.site_id = site
-            for col in [c for c in site_df.columns if c != 'site_id']:
-                site_df[col] = site_df[col].interpolate(limit_direction='both', method='linear')
-                site_df[col] = site_df[col].fillna(df[col].median())
-            site_dfs.append(site_df)
-        df = pd.concat(site_dfs)
-        df['timestamp'] = df.index
-        df = df.reset_index(drop=True)
-
-    if add_lag:
-        df = add_lag_features(df, window=3)
-
-    return reduce_mem(df)
-
-
-def merged_dfs(source='train', fix_timezone=True, impute=True, add_lag=False):
-    df = load_data(source=source).merge(load_building(), on='building_id', how='left')
-    df = df.merge(load_weather(source=source, fix_timezone=fix_timezone, impute=impute, add_lag=add_lag),
-                  on=['site_id', 'timestamp'], how='left')
-    if source == 'train':
-        X = df.drop('meter_reading', axis=1)
-        y = np.log1p(df.meter_reading)  # log-transform of target
-        return X, y
-    elif source == 'test':
-        return df
